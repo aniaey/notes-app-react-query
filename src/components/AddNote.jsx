21907668bf31d668b3ffe071a7373d19c8c5eb64
Ania Banachowicz <Anna.Banachowicz@gds.ey.com>
2025-02-12 15:47:06 +0100

    Added base styles and layout for components, implemented pinning the notes, used React Query

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,5 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+
 const AddNote = () => {
-  return <>Add note</>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const queryClient = useQueryClient();
+
+  const addNewNoteMutation = useMutation({
+    mutationFn: async (newNote) => {
+      return await axios.post("http://localhost:3000/notes", { newNote });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["notes"]);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addNewNoteMutation.mutate({
+      title: title,
+      content: description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <div className="card bg-primary-subtle">
+      <form onSubmit={handleSubmit}>
+        <div className="card-body form-group d-flex flex-column gap-2">
+          <input
+            className="form-control"
+            type="text"
+            placeholder={"Title"}
+            aria-label={"Title"}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            style={{
+              height: "80px",
+            }}
+            className="form-control"
+            placeholder="Take a note ..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button type="button" className="btn btn-outline-dark">
+            Add Note
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddNote;
--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -1,5 +1,47 @@
-const EditNote = (/*{ note, onSave }*/) => {
-  return <>Edit note </>;
+import axios from "axios";
+import { useState } from "react";
+
+const EditNote = ({ note }) => {
+  const [newTitle, setNewTitle] = useState(note.title);
+  const [newDescription, setNewDescription] = useState(note.content);
+
+  const saveNote = async () => {
+    return await axios.put(`http://localhost:3000/notes/${note.id}`, {
+      title: newTitle,
+      content: newDescription,
+    });
+  };
+
+  return (
+    <div className="card mb-2">
+      <form onSubmit={saveNote}>
+        <div className="card-body d-flex flex-column gap-2">
+          <input
+            className="form-control"
+            type="text"
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <textarea
+            style={{
+              height: "80px",
+            }}
+            className="form-control"
+            placeholder="Comments"
+            value={newDescription}
+            onChange={(e) => setNewDescription(e.target.value)}
+          />
+          <button
+            type="button"
+            className="btn bg-secondary-subtle"
+            onClick={saveNote}
+          >
+            Save changes
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default EditNote;
